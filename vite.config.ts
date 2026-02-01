c2a6b5fb7c7a35b434d2d2d18fea34849b0a705b
sialaichai <sialaichai@gmail.com>
2026-02-01 21:57:01 +0800

    wave

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
+import { fileURLToPath } from 'url';
 import path from "path"
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
-import { inspectAttr } from 'kimi-plugin-inspect-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: '/wave-invaders/',
-  plugins: [inspectAttr(), react()],
+  base: './',
+  plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./src"),
+      "@": fileURLToPath(new URL('./src', import.meta.url)),
     },
   },
 });
